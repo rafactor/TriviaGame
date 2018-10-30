e1356e8545e3706c257be5ba9ead4372a2fc1402
rafactor <avila.rafael@gmail.com>
2018-10-30 07:16:55 -0400

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+/* 
+* LIST OF ALL QUESTIONS
+*/
+
+var question = [
+    {
+    'question': "Question 1",
+    'alternatives' : ['answer a','answer b','answer c'],
+    'answer': 'answer b'},
+    {
+    'question': "Question 2",
+    'alternatives' : ['answer a','answer b','answer c'],
+    'answer': 'answer a'},
+    {
+        'question': "Question 3",
+        'alternatives' : ['answer a','answer b','answer c'],
+        'answer': 'answer c'}, 
+]
+
+/* 
+* LIST OF ALL QUESTIONS
+*/
+
