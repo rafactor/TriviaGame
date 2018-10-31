d0b66f46826045a6509ea8985ac1d6c8bad5370b
rafactor <avila.rafael@gmail.com>
2018-10-30 23:04:55 -0400

    card structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,6 +18,41 @@ var question = [
 ]
 
 /* 
-* LIST OF ALL QUESTIONS
+* Create the HTML Structure
 */
+
+// Global variables
+var numberOfQuestions = 10; //number of questions that the user will answer
+var questionNumber = 0;
+var rightAnswers = 0;
+var wrongAnswers = 0;
+var $card = $('.card');
+
+// Create the initial screen with the start button
+
+// Create the listener for the start button
+
+
+// Build the template with question, alternatives and timer
+
+// create the timer
+
+// Populate the template with the first question, alternatives and timer
+
+// create the listener for the response
+
+// Compare the user response with the question answer
+
+// Timer function that will mark the question as wrong if the timer is over
+
+// Calculate the score (right and wrong)
+
+// Update the screen accordingly to the response (right or wrong)
+
+// check if there is another question to be shown. 
+
+// If yes, show a button to show the right answer or load it right away
+
+// if not, show the final score
+
 
